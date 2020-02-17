7a1156d9cd58e3068b4bb7c395d637dd2ad25519
Micah Mills <micahmills@gmail.com>
2020-02-17 14:39:11 +0300

    changed the i18n function to not modify a global.

--- disciple-tools-starter-plugin.php
+++ disciple-tools-starter-plugin.php
@@ -253,9 +253,20 @@ class RSDT_Plugin {
      * @return void
      */
     public function i18n() {
-        global $locale;
-        $locale = get_user_locale();
-        load_plugin_textdomain( 'RSDT', false, trailingslashit( dirname( plugin_basename( __FILE__ ) ) ). 'languages/' );
+            //Take from loadTextDomain() in /disciple-tools-theme/dt-core/libraries/plugin-update-checker/Puc/v4p5/UpdateChecker.php
+			$domain = 'RSDT';
+			$locale = apply_filters(
+				'plugin_locale',
+				(is_admin() && function_exists('get_user_locale')) ? get_user_locale() : get_locale(),
+				$domain
+			);
+
+			$moFile = $domain . '-' . $locale . '.mo';
+            $path = realpath(dirname(__FILE__) . '/languages');
+
+			if ($path && file_exists($path)) {
+				load_textdomain($domain, $path . '/' . $moFile);
+			}
     }
 
     /**
